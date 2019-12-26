903d605f9fd6523cadce90a7324db75356741cd7
neuli <juergen.neugschwandtner@gmx.at>
2019-12-26 22:20:22 +0100

    added source + appveyor.yml

---
+++ src/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int TestFunction()
+        {
+#if NETSTANDARD2_0 || NETSTANDARD2_1
+            return 3;
+#elif NET461 || NET462
+            return 8;
+#else
+            return 4;
+#endif
+        }
+    }
+}
---
+++ src/TestProject/Class1.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("DF");
+        }
+        [Test]
+        public void MyTestMethod()
+        {
+            Assert.AreEqual(8, new ClassLibrary1.Class1().TestFunction());
+        }
+
+        [Test]
+        public void TestLineEndings()
+        {
+            string testString = @"Das ist CRLF
+Zweite Zeile";
+            string expected = "Das ist CRLF\r\nZweite Zeile";
+
+            Assert.AreEqual(expected, testString);
+        }
+    }
+}
